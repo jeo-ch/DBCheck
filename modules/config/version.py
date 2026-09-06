14c7d93826222478f61d444be1093b9dd3b860f2
973721777 <973721777@qq.com>
2026-09-06 19:33:47 +0800

    Release v26.9.6.0

--- modules/config/version.py
+++ modules/config/version.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2025-2026 fiyo (Jack Ge) <sdfiyon@gmail.com>
 # Author: fiyo (Jack Ge) - https://github.com/fiyo/DBCheck
